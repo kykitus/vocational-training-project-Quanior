c4b37693271bd6ac352544c621da868769b6ecb3
Kitus <130890471+kykitus@users.noreply.github.com>
2025-02-21 08:07:56 +0100

    jo

---
+++ Release/SerialBT.ino
@@ -0,0 +1,54 @@
+#include "Wire.h"
+#include <MPU6050_light_better.h>
+#include "BluetoothSerial.h"
+
+MPU6050 mpu(Wire);
+
+BluetoothSerial SerialBT;
+
+String result = "";
+unsigned long timer = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+   while (!Serial) 
+  {
+    delay(10);
+  }
+
+
+  SerialBT.begin("QuackPLank"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while(status!=0){ } // stop everything if could not connect to MPU6050
+  
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  mpu.calcOffsets(true,true); // gyro and accelero
+  Serial.println("Done!\n");
+}
+
+void loop() {
+
+  mpu.update();
+  if(millis() - timer > 10)
+  { // print data every second
+  result.concat(mpu.getAngleX());
+  result.concat(" ");
+  result.concat(mpu.getAngleY());
+  result.concat(" ");
+  result.concat(mpu.getAngleZ());
+
+  SerialBT.println(result);
+  result = "";
+  timer = millis();
+  }
+  //delay(20);
+}
