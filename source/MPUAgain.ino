40a574d76c069988be026249bd0b0aafe36300b0
Kitus <kitus.versa@gmail.com>
2025-02-25 10:09:24 +0100

    l

--- source/MPUAgain.ino
+++ source/MPUAgain.ino
@@ -1,20 +1,34 @@
 #include <Arduino.h>
 #include <TinyMPU6050.h>
 #include "BluetoothSerial.h"
+#include "Wire.h"
+#include "esp_system.h"
+
+//RTC_NOINIT_ATTR bool bootflag = true; Linie zakomentowane są jedną z prób użycia restartu ESP do poprawienia działania MPU. Restart działa, ale MPU zachowuje się tak samo.
 
 BluetoothSerial SerialBT;
 
-MPU6050 mpu (Wire);
+MPU6050 mpu(Wire);
 
 void setup()
 {
+  //delay(1000);
+
+  Serial.begin(115200);
+  
+  /*
+  if (bootflag != false)
+  {
+    Serial.println(bootflag);
+    Serial.println("Resetting tour");
+    bootflag = false;
+    //esp_restart();  // More reliable than ESP.restart()
+  }*/
+
   // Initialization
   mpu.Initialize();
-  Serial.begin(115200);
-  SerialBT.begin("QuackPLank"); //Bluetooth device name
+  SerialBT.begin("QuackPLank");
 
-  // Calibration
-  
   Serial.println("=====================================");
   Serial.println("Starting calibration...");
   mpu.Calibrate();
